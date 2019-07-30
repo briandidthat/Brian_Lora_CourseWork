50f47a4f3f6fb259bec3089e12758de656301c53
Blora616 <Blora616@gmail.com>
2019-07-30 09:14:29 -0400

    Hi-Lo

--- PBDs/Brian_Lora_U1M2L1PBD/src/main/java/com/company/HiLo.java
+++ PBDs/Brian_Lora_U1M2L1PBD/src/main/java/com/company/HiLo.java
@@ -10,7 +10,34 @@ import java.util.Scanner;
 
 public class HiLo {
 
-    public static void main(String[] args) {
+    public static void returnAnswer(boolean ans, int guess) {
+        if (ans) {
+            System.out.println("That's right!  My secret number was " + guess + "!");
 
+        } else {
+            System.out.println("Your guess: " + guess);
+            System.out.println("Sorry, but I was really thinking of another number. Try again.");
+        }
+    }
+    public static void playGame() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Welcome to the guessing game! Please pick a number from 1-100.");
+
+        // must initialize counter at 1 since the first input will not be inside loop
+        int counter = 1;
+        int maxGuesses = 7;
+        int guess = in.nextInt();
+
+        while (guess != maxGuesses) {
+            returnAnswer(false, guess);
+            guess = in.nextInt();
+            counter +=1;
+        }
+        returnAnswer(true, guess);
+        System.out.println("It only took you " +counter+ " tries.");
+    }
+
+    public static void main(String[] args) {
+        playGame();
     }
 }
