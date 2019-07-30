76147d53587ac073f2ce0779a34a7809d9e6d0a3
Blora616 <Blora616@gmail.com>
2019-07-30 19:24:02 -0400

    complete refactored calculator assignment

--- Katas/20190731/U1M2L4Katas/Calculator/src/main/java/com/company/Calculator.java
+++ Katas/20190731/U1M2L4Katas/Calculator/src/main/java/com/company/Calculator.java
@@ -2,20 +2,70 @@ package com.company;
 
 public class Calculator {
 
+    // ADD METHODS
     public int add(int a, int b) {
         return a + b;
     }
 
+    public double add( double a, double b) {
+        return  a + b;
+    }
+
+    public float add( float a, float b) {
+        return  a + b;
+    }
+
+    public long add( long a, long b) {
+        return  a + b;
+    }
+
+    // SUBTRACT METHODS
     public int subtract(int a, int b) {
         return a - b;
     }
+    public double subtract(double a, double b) {
+        return  a - b;
+    }
 
+    public float subtract(float a, float b) {
+        return  a - b;
+    }
+
+    public long subtract(long a, long b) {
+        return  a - b;
+    }
+
+    // MULTIPLY METHODS
     public int multiply(int a, int b) {
         return a * b;
     }
 
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public float multiply(float a, float b) {
+        return a * b;
+    }
+
+    public long multiply(long a, long b) {
+        return a * b;
+    }
+
+    // DIVIDE METHODS
     public int divide(int a, int b) {
         return a / b;
     }
 
+    public double divide(double a, double b) {
+        return a / b;
+    }
+
+    public float divide(float a, float b) {
+        return a / b;
+    }
+
+    public long divide(long a, long b) {
+        return a / b;
+    }
 }
