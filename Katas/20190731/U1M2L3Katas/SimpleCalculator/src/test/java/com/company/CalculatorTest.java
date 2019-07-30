3e086c0e897be76f9896fc70c9e3eed984397110
Blora616 <Blora616@gmail.com>
2019-07-30 19:12:25 -0400

    complete Calculator assignment

--- Katas/20190731/U1M2L3Katas/SimpleCalculator/src/test/java/com/company/CalculatorTest.java
+++ Katas/20190731/U1M2L3Katas/SimpleCalculator/src/test/java/com/company/CalculatorTest.java
@@ -8,43 +8,43 @@ public class CalculatorTest {
 
     private Calculator calc = new Calculator();
 
-    // @Test
-    // public void testAdd() {
+     @Test
+     public void testAdd() {
 
-    //     String failMessage = "Expected calculator to handle integer addition.";
+         String failMessage = "Expected calculator to handle integer addition.";
 
-    //     assertEquals(failMessage, 5, calc.add(2, 3));
-    //     assertEquals(failMessage, 17, calc.add(20, -3));
-    //     assertEquals(failMessage, 1, calc.add(0, 1));
-    // }
+         assertEquals(failMessage, 5, calc.add(2, 3));
+         assertEquals(failMessage, 17, calc.add(20, -3));
+         assertEquals(failMessage, 1, calc.add(0, 1));
+     }
 
-    // @Test
-    // public void testSubtract() {
+     @Test
+     public void testSubtract() {
 
-    //     String failMessage = "Expected calculator to handle integer subtraction.";
+         String failMessage = "Expected calculator to handle integer subtraction.";
 
-    //     assertEquals(failMessage, -1, calc.subtract(2, 3));
-    //     assertEquals(failMessage,  23, calc.subtract(20, -3));
-    //     assertEquals(failMessage, 10, calc.subtract(11, 1));
-    // }
+         assertEquals(failMessage, -1, calc.subtract(2, 3));
+         assertEquals(failMessage,  23, calc.subtract(20, -3));
+         assertEquals(failMessage, 10, calc.subtract(11, 1));
+     }
 
-    // @Test
-    // public void testMultiply() {
+     @Test
+     public void testMultiply() {
 
-    //     String failMessage = "Expected calculator to handle integer multiplication.";
+         String failMessage = "Expected calculator to handle integer multiplication.";
 
-    //     assertEquals(failMessage, 6, calc.multiply(2, 3));
-    //     assertEquals(failMessage, -60, calc.multiply(20, -3));
-    //     assertEquals(failMessage, 0, calc.multiply(0, 1));
-    // }
+         assertEquals(failMessage, 6, calc.multiply(2, 3));
+         assertEquals(failMessage, -60, calc.multiply(20, -3));
+         assertEquals(failMessage, 0, calc.multiply(0, 1));
+     }
 
-    // @Test
-    // public void testDivide() {
+     @Test
+     public void testDivide() {
 
-    //     String failMessage = "Expected calculator to handle integer division.";
+         String failMessage = "Expected calculator to handle integer division.";
 
-    //     assertEquals(failMessage, 1, calc.divide(4, 3));
-    //     assertEquals(failMessage, -5, calc.divide(20, -4));
-    //     assertEquals(failMessage, 3, calc.divide(18, 5));
-    // }
+         assertEquals(failMessage, 1, calc.divide(4, 3));
+         assertEquals(failMessage, -5, calc.divide(20, -4));
+         assertEquals(failMessage, 3, calc.divide(18, 5));
+     }
 }
