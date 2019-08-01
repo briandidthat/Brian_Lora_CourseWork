feb9da6c4c048ce4eb456d91a2414794d6a6b2d2
Blora616 <Blora616@gmail.com>
2019-08-01 15:31:10 -0400

    initial commit

---
+++ PBDs/Brian_Lora_U1M2L7PBD/src/main/java/com/company/BabyBlackjack.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/*Task:
+ * Write a program that allows a human user to play a single hand of "blackjack" against a dealer.
+ * Pick two values from 1-10 for the player. These are the player's "cards".
+ * Pick two more values from 1-10 for the dealer. Whoever has the highest total is the winner.
+ * There is no betting, no busting, and no hitting. Save that for real blackjack.
+ */
+public class BabyBlackjack {
+
+}
---
+++ PBDs/Brian_Lora_U1M2L7PBD/src/main/java/com/company/NoticingEvenNumbers.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/*Task:
+ *Write a program that uses a for loop to display all the numbers from 1 to 20, marking those which are
+ * even (divisible by two). It should use modulus by 2: if the remainder is zero, it's divisible by 2.
+ * This means you'll need an if statement inside the loop.
+ */
+public class NoticingEvenNumbers {
+
+}
