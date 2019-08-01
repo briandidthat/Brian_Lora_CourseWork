68fd9451cc2c828e389d80963be636e878b0648b
Blora616 <Blora616@gmail.com>
2019-08-01 09:11:12 -0400

    fix righty

--- PBDs/Brian_Lora_U1M2L5PBD/src/main/java/com/company/RightTriangleChecker.java
+++ PBDs/Brian_Lora_U1M2L5PBD/src/main/java/com/company/RightTriangleChecker.java
@@ -13,7 +13,36 @@ public class RightTriangleChecker {
         return in.nextInt();
     }
 
-    public static void main(String[] args) {
+    public static void isValidTriangle() {
+        int a,b,c;
+        System.out.println("Enter three integers:");
 
+        System.out.print("Side 1:");
+        a = getInput();
+
+        do{
+            System.out.print("Side 2:");
+            b = getInput();
+            if (b < a)
+                System.out.println(b+ " is smaller than " + a +". Try again.");
+        } while (b < a);
+
+        do{
+            System.out.print("Side 3:");
+            c = getInput();
+            if (c < b)
+                System.out.println(c+ " is smaller than " + b +". Try again.");
+        }while (c < b);
+
+        System.out.println("\nYour three sides are: " +a+" "+b+" "+c);
+        if (a*a + b*b == c*c){
+            System.out.println("Wee! These sides make a right triangle!");
+        }else{
+            System.out.println("NO! these sides do NOT make a right triangle.");
+        }
+    }
+
+    public static void main(String[] args) {
+        isValidTriangle();
     }
 }
