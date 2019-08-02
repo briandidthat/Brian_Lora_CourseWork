a04f28ae72b96e69fd3f5c92cdbcd5ea616656b0
Blora616 <Blora616@gmail.com>
2019-08-02 07:33:23 -0400

    checkEvens

--- PBDs/Brian_Lora_U1M2L7PBD/src/main/java/com/company/NoticingEvenNumbers.java
+++ PBDs/Brian_Lora_U1M2L7PBD/src/main/java/com/company/NoticingEvenNumbers.java
@@ -6,8 +6,17 @@ package com.company;
  * This means you'll need an if statement inside the loop.
  */
 public class NoticingEvenNumbers {
+    public static void checkEven() {
+        for (int i =0; i < 21; i++) {
+            if(i % 2 == 0) {
+                System.out.println(i + " *******");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
 
     public static void main(String[] args) {
-
+        checkEven();
     }
 }
