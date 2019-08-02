d90d195f98cf794e012c6499b1844af3a0452391
Blora616 <Blora616@gmail.com>
2019-08-02 13:51:56 -0400

    add files

--- PBDs/Brian_Lora_U1M2L8PBD/src/main/java/com/company/BabyNim.java
+++ PBDs/Brian_Lora_U1M2L8PBD/src/main/java/com/company/BabyNim.java
@@ -1,8 +1,15 @@
 package com.company;
-
+import java.util.Scanner;
 /*Task:
- *
- */
+* Write a program that starts with three "piles" of 3 counters each. Let the player choose piles and remove
+* counters until all the piles are empty.Start by placing counters (coins or toothpicks or something) into 3 piles.
+* The player picks a pile, then removes one or more counters from that pile. (It's okay to take the whole pile.)
+* The player picks a new pile, then removes one or more counters from that pile.
+* (It's okay to pick the same pile as before.) Once all piles are empty, the game stops.
+* You do not need to check for errors like a wrong pile name, or if someone tries to take more counters
+* from the pile than the pile has.
+*/
+
 public class BabyNim {
 
 }
--- PBDs/Brian_Lora_U1M2L8PBD/src/main/java/com/company/Nim.java
+++ PBDs/Brian_Lora_U1M2L8PBD/src/main/java/com/company/Nim.java
@@ -1,8 +1,15 @@
 package com.company;
-
+import java.util.Scanner;
 /*Task:
- *
- */
+* Nim is a strategy game between two players.
+* Start by placing counters (coins or toothpicks or something) into 3 piles.
+* Player #1 picks a pile, then removes one or more counters from that pile. (It's okay to take the whole pile.)
+* Player #2 picks a pile, then removes one or more counters from that pile.
+* Player #1 plays again. (It's okay to choose a different pile this time.)
+* Whichever player is forced to take the last counter is the LOSER.
+* Write a program that allows two human players to play Nim against each other. The program should detect when
+* the last counter has been taken and declare a winner.
+*/
 public class Nim {
 
 }
