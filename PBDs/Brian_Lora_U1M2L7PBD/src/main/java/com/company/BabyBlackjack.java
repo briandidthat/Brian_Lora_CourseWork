7f804189816e64cc1f53d439c40431c3777b2968
Blora616 <Blora616@gmail.com>
2019-08-01 22:12:57 -0400

    blackjack

--- PBDs/Brian_Lora_U1M2L7PBD/src/main/java/com/company/BabyBlackjack.java
+++ PBDs/Brian_Lora_U1M2L7PBD/src/main/java/com/company/BabyBlackjack.java
@@ -1,4 +1,6 @@
 package com.company;
+import java.util.Scanner;
+import java.util.Random;
 
 /*Task:
  * Write a program that allows a human user to play a single hand of "blackjack" against a dealer.
@@ -8,4 +10,36 @@ package com.company;
  */
 public class BabyBlackjack {
 
+    public static int generateRandomInt() {
+        Random random = new Random();
+        return random.nextInt(10+1);
+    }
+
+    public static String calculateSum() {
+        int p1 = generateRandomInt();
+        int p2 = generateRandomInt();
+        System.out.println("Your cards: "+ p1 + " " +  p2);
+
+        int d1 = generateRandomInt();
+        int d2 = generateRandomInt();
+        System.out.println("Dealer's cards: "+ d1 +" "+  d2);
+
+        int playerTotal = p1 + p2;
+        int dealerTotal = d1 + d2;
+
+        if (playerTotal > dealerTotal) {
+            return "Player won!";
+        } else {
+            return "Dealer won!";
+        }
+    }
+
+    public static void playGame() {
+        System.out.println("WELCOME TO BLACKJACK!!!\n Dealer shuffling......\n Bets taken. Cards coming up\n");
+        System.out.println(calculateSum() );
+    }
+
+    public static void main(String[] args) {
+        playGame();
+    }
 }
