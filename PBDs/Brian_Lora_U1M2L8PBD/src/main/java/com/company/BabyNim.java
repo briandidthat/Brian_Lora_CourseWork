defc9d2e746f7e02372c9f9d7b14c1d81fb13475
Blora616 <Blora616@gmail.com>
2019-08-03 17:05:17 -0400

    readme

--- PBDs/Brian_Lora_U1M2L8PBD/src/main/java/com/company/BabyNim.java
+++ PBDs/Brian_Lora_U1M2L8PBD/src/main/java/com/company/BabyNim.java
@@ -1,5 +1,6 @@
 package com.company;
 import java.util.Scanner;
+
 /*Task:
 * Write a program that starts with three "piles" of 3 counters each. Let the player choose piles and remove
 * counters until all the piles are empty.Start by placing counters (coins or toothpicks or something) into 3 piles.
@@ -9,7 +10,33 @@ import java.util.Scanner;
 * You do not need to check for errors like a wrong pile name, or if someone tries to take more counters
 * from the pile than the pile has.
 */
-
 public class BabyNim {
 
+    public static int handleInput() {
+        Scanner in = new Scanner(System.in);
+        return in.nextInt();
+    }
+    //arbitrary func
+    public static int checkPile(int a , int b) {
+        return a + b;
+    }
+
+    public static void playGame() {
+        int selection = handleInput();
+        int counter1 = 3;
+        int counter2 = 3;
+        int counter3 = 3;
+
+        while (counter1 !=0 && counter2 != 0 && counter3 !=0) {
+            System.out.format("A: %d\tB: %d\tC: %d \n", counter1, counter2, counter3);
+            selection = handleInput();
+
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Baby Nim! Pick a pile to remove from. ");
+        System.out.println("A: 3\tB: 3\tC: 3");
+        playGame();
+    }
 }
--- PBDs/Brian_Lora_U1M2L8PBD/src/main/java/com/company/Nim.java
+++ PBDs/Brian_Lora_U1M2L8PBD/src/main/java/com/company/Nim.java
@@ -1,5 +1,6 @@
 package com.company;
 import java.util.Scanner;
+
 /*Task:
 * Nim is a strategy game between two players.
 * Start by placing counters (coins or toothpicks or something) into 3 piles.
@@ -7,9 +8,18 @@ import java.util.Scanner;
 * Player #2 picks a pile, then removes one or more counters from that pile.
 * Player #1 plays again. (It's okay to choose a different pile this time.)
 * Whichever player is forced to take the last counter is the LOSER.
-* Write a program that allows two human players to play Nim against each other. The program should detect when
-* the last counter has been taken and declare a winner.
+* Write a program that allows two human players to play Nim against each other.
+* The program should detect when the last counter has been taken and declare a winner.
 */
 public class Nim {
+
+    public static int handleInput() {
+        Scanner in = new Scanner(System.in);
+        return in.nextInt();
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 }
