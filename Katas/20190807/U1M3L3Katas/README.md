3ed2b4b7cc554126a6ffbe309c3fe68cc6f46b2c
Blora616 <Blora616@gmail.com>
2019-08-06 13:27:12 -0400

    complete lesson3 katas

--- Katas/20190807/U1M3L3Katas/README.md
+++ Katas/20190807/U1M3L3Katas/README.md
@@ -11,7 +11,8 @@ Handle the following exceptions:
 
 1. When a user enters a non-integer value, display "Please enter a valid number."
 1. When a user chooses a value not present in the array, display "Please choose an available pet."
-1. If the file does not exist, display the message "The following file does not seem to exist:" along with the output from the Exception `.getMessage()`.
+1. If the file does not exist, display the message "The following file does not seem to exist:" 
+along with the output from the Exception `.getMessage()`.
 1. If there was trouble writing the file, display "Something went wrong while writing your file."
 1. Regardless of success or exceptions, tell the user to "Have a nice day!"
 
--- Katas/20190807/U1M3L3Katas/src/main/java/com/company/Application.java
+++ Katas/20190807/U1M3L3Katas/src/main/java/com/company/Application.java
@@ -1,6 +1,9 @@
 package com.company;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.WriteAbortedException;
+import java.rmi.NoSuchObjectException;
 
 public class Application {
 
@@ -18,11 +21,18 @@ public class Application {
 
             System.out.println("New saved animals file:");
             Pets.readPetsFromFile();
-        }catch (IOException e){
+        } catch (NumberFormatException e) {
+            System.out.println("Please enter a valid number." + e.getMessage());
+        } catch (FileNotFoundException e) {
+            System.out.println("The following file does not seem to exist: " + e.getMessage());
+        } catch (WriteAbortedException e) {
+            System.out.println("Something went wrong while writing your file.");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Please choose an available pet.");
+        } catch (IOException e) {
             System.out.println("This is an error: " + e);
+        } finally {
+            System.out.println("Have a nice day!");
         }
-
-
-
     }
 }
