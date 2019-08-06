21a82b6c3d2b9b8ed14f1424f24d30598fe8c991
Blora616 <Blora616@gmail.com>
2019-08-05 20:54:13 -0400

    init commit LargestCities

---
+++ Additional_Assignments/LargestCities/src/main/java/com/company/City.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+
+/*Task:
+ *
+ */
+public class City {
+    private String name;
+    private int population;
+
+    public String handleInput() {
+        Scanner in = new Scanner(System.in);
+        return in.nextLine();
+    }
+
+
+}
