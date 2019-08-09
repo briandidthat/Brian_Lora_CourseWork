3e0fcea6ccbf3c66656d7176d076277419feff53
Blora616 <Blora616@gmail.com>
2019-08-08 22:18:59 -0400

    finish pbd's

--- PBDs/Brian_Lora_U1M3L6PBD/src/main/java/com/company/GettingTheLargestValue.java
+++ PBDs/Brian_Lora_U1M3L6PBD/src/main/java/com/company/GettingTheLargestValue.java
@@ -1,8 +1,40 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /*Task:
- *
- */
+* Write a program that creates an ArrayList which can hold Integers. Fill the ArrayList with random numbers
+* from 1 to 100. Display the values in the ArrayList on the screen. Then use a linear search to find the largest value
+* in the ArrayList, and display that value.
+*/
+
 public class GettingTheLargestValue {
 
+    public static int generateRandom() {
+        Random random = new Random();
+        return random.nextInt(100) + 1;
+    }
+
+    public static int getLargest(List<Integer> newList) {
+        int largest = Integer.MIN_VALUE;
+
+        for (int i =0; i < newList.size(); i++) {
+            if(newList.get(i) > largest) {
+                largest = newList.get(i);
+            }
+        }
+        return largest;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> numList = new ArrayList<Integer>();
+
+        for (int i =0; i< 10; i++) {
+            numList.add(generateRandom());
+        }
+        System.out.println("ArrayList: " + numList);
+        System.out.format("The largest value is %d.", getLargest(numList));
+    }
 }
--- PBDs/Brian_Lora_U1M3L6PBD/src/main/java/com/company/LocatingTheLargestValue.java
+++ PBDs/Brian_Lora_U1M3L6PBD/src/main/java/com/company/LocatingTheLargestValue.java
@@ -1,8 +1,41 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 /*Task:
- *
- */
-public class LocatingTheLargestValue {
+* Write a program that creates an ArrayList of Integers. Fill the ArrayList with random numbers from 1 to 100.
+* Display the values in the ArrayList on the screen. Then use a linear search to find the largest value
+* in the ArrayList, and display that value and its slot number.
+*/
 
+public class LocatingTheLargestValue {
+    public static int generateRandom() {
+        Random random = new Random();
+        return random.nextInt(100) + 1;
+    }
+    public static void printLargestIndex(List<Integer> numList) {
+        int largest = Integer.MIN_VALUE;
+        int largestIdx = 0;
+
+        for (int i =0; i < numList.size(); i++) {
+            if(numList.get(i) > largest) {
+                largest = numList.get(i);
+                largestIdx = numList.indexOf(largest);
+            }
+        }
+
+        System.out.format("The largest value is %d, which is in slot %d.", largest,largestIdx);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> numList = new ArrayList<Integer>();
+
+        for (int i =0; i< 10; i++) {
+            numList.add(generateRandom());
+        }
+
+        System.out.println("ArrayList: " +numList);
+        printLargestIndex(numList);
+    }
 }
