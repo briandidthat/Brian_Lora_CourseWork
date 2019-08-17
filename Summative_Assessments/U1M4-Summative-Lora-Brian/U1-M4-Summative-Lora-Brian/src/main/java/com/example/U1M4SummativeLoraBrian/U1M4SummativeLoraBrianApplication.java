520609ae8774e1dabc0f6f7e1ed2114192f1a5e3
Blora616 <Blora616@gmail.com>
2019-08-16 20:20:04 -0400

    init commit

---
+++ Summative_Assessments/U1M4-Summative-Lora-Brian/U1-M4-Summative-Lora-Brian/src/main/java/com/example/U1M4SummativeLoraBrian/U1M4SummativeLoraBrianApplication.java
@@ -0,0 +1,13 @@
+package com.example.U1M4SummativeLoraBrian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U1M4SummativeLoraBrianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U1M4SummativeLoraBrianApplication.class, args);
+	}
+
+}
