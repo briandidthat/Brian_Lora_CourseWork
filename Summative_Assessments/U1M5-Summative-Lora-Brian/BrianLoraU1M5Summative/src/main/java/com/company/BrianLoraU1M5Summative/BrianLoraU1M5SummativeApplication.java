730a6e369ff7f48c203becf8120d65db5ace4559
Blora616 <Blora616@gmail.com>
2019-08-23 18:18:23 -0400

    initial commit

---
+++ Summative_Assessments/U1M5-Summative-Lora-Brian/BrianLoraU1M5Summative/src/main/java/com/company/BrianLoraU1M5Summative/BrianLoraU1M5SummativeApplication.java
@@ -0,0 +1,13 @@
+package com.company.BrianLoraU1M5Summative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrianLoraU1M5SummativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrianLoraU1M5SummativeApplication.class, args);
+	}
+
+}
