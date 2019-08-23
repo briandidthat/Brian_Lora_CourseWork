b929b59f1f173bace142b11206e91882fedb8929
Blora616 <Blora616@gmail.com>
2019-08-23 10:00:29 -0400

    final

--- Summative_Assessments/U1M4-Summative-Lora-Brian/U1-M4-Summative-Lora-Brian/src/main/java/com/example/U1M4SummativeLoraBrian/controller/MagicEightBallController.java
+++ Summative_Assessments/U1M4-Summative-Lora-Brian/U1-M4-Summative-Lora-Brian/src/main/java/com/example/U1M4SummativeLoraBrian/controller/MagicEightBallController.java
@@ -2,11 +2,6 @@ package com.example.U1M4SummativeLoraBrian.controller;
 
 import com.example.U1M4SummativeLoraBrian.model.Magic;
 import com.example.U1M4SummativeLoraBrian.model.MagicList;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import jdk.nashorn.internal.parser.JSONParser;
-import org.json.JSONException;
-import org.springframework.boot.jackson.JsonObjectDeserializer;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import java.util.Random;
@@ -19,8 +14,9 @@ public class MagicEightBallController {
     public Magic postQuestion(@RequestBody String question) {
         MagicList magicList = new MagicList();
         Magic magic = magicList.getMagic(generateRandom());
-        // is there  a better way to achieve this so I dont have to do all of this voodoo? If so, please provide a link
-        // or a method. Thank you!
+        // is there  a better way to achieve this so that I dont have to do all of this voodoo? If so,
+        // please provide a link or a method name. Thank you!
+
         question = question.replaceAll("\\{", "").replaceAll("\\}","")
                 .replaceAll("question", "").replaceAll("\"", "")
                 .replaceAll(":", "");
