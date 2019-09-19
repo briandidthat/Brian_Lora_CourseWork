617f110bb02d460a22b9b96f8d2145e124073fde
Blora616 <Blora616@gmail.com>
2019-09-19 15:05:32 -0400

    initial commit

---
+++ Additional_Assignments/GlossaryProject/trilogy-thesauras/src/main/java/com/trilogyed/trilogythesauras/TrilogyThesaurasApplication.java
@@ -0,0 +1,13 @@
+package com.trilogyed.trilogythesauras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrilogyThesaurasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrilogyThesaurasApplication.class, args);
+	}
+
+}
