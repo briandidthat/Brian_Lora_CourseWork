cfdb82263beae89b7642c819b1e3ab4841848b42
Blora616 <Blora616@gmail.com>
2019-09-20 14:44:58 -0400

    intialize

---
+++ Additional_Assignments/Hotel-Booking-Project/hotel-booking-service/src/main/java/com/trilogyed/hotelbookingservice/HotelBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trilogyed.hotelbookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingServiceApplication.class, args);
+	}
+
+}
