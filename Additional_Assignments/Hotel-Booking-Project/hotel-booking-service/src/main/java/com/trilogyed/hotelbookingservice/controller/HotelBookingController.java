a62173daaba56b2829c9233c1a936c41390e3b58
Blora616 <Blora616@gmail.com>
2019-09-20 15:59:29 -0400

    client

--- Additional_Assignments/Hotel-Booking-Project/hotel-booking-service/src/main/java/com/trilogyed/hotelbookingservice/controller/HotelBookingController.java
+++ Additional_Assignments/Hotel-Booking-Project/hotel-booking-service/src/main/java/com/trilogyed/hotelbookingservice/controller/HotelBookingController.java
@@ -1,11 +1,17 @@
 package com.trilogyed.hotelbookingservice.controller;
 
+import com.trilogyed.hotelbookingservice.viewmodel.HotelBookingViewModel;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/*Task:
- *
- */
+
 @RestController
 public class HotelBookingController {
-
+    @RequestMapping(path = {"/hotelRewards/{roomId}?rewardsMember={rewardsMember}&doubleBonusDay={doubleBonusDay}", "/hotelRewards"})
+    public HotelBookingViewModel getRates(@PathVariable("roomId") int roomId,
+                                          @PathVariable(name = "rewardsMember", required = false) boolean rewardsMember,
+                                          @PathVariable(name = "doubleBonusDay", required = false) boolean doubleBonusDay) {
+        return null;
+    }
 }
--- Additional_Assignments/Hotel-Booking-Project/hotel-booking-service/src/main/java/com/trilogyed/hotelbookingservice/service/HotelBookingService.java
+++ Additional_Assignments/Hotel-Booking-Project/hotel-booking-service/src/main/java/com/trilogyed/hotelbookingservice/service/HotelBookingService.java
@@ -1,8 +1,29 @@
 package com.trilogyed.hotelbookingservice.service;
 
-/*Task:
- *
- */
-public class HotelBookingService {
+import com.trilogyed.hotelbookingservice.model.HotelBooking;
+import com.trilogyed.hotelbookingservice.model.Rewards;
+import com.trilogyed.hotelbookingservice.model.Room;
+import com.trilogyed.hotelbookingservice.util.feign.RewardsClient;
+import com.trilogyed.hotelbookingservice.util.feign.RoomClient;
+import com.trilogyed.hotelbookingservice.viewmodel.HotelBookingViewModel;
+import org.springframework.beans.factory.annotation.Autowired;
 
+
+public class HotelBookingService {
+    private RewardsClient rewardsClient;
+    private RoomClient roomClient;
+
+    @Autowired
+    public HotelBookingService(RewardsClient rewardsClient, RoomClient roomClient) {
+        this.rewardsClient = rewardsClient;
+        this.roomClient = roomClient;
+    }
+
+    private HotelBookingViewModel buildHotelBookViewModel(HotelBooking hotelBooking, Room room, Rewards rewards) {
+        HotelBookingViewModel hbvm = new HotelBookingViewModel();
+        hbvm.setRoomId(room.getNumber());
+        hbvm.setRewardsMember(hotelBooking.isRewardsMember());
+
+        return hbvm;
+    }
 }
