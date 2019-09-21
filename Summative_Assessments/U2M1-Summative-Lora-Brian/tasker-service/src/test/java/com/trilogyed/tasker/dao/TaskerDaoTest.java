c60cfdf8bb769025fd88532cad50f8e21075935d
Blora616 <Blora616@gmail.com>
2019-09-20 21:28:21 -0400

    secrets

--- Summative_Assessments/U2M1-Summative-Lora-Brian/tasker-service/src/test/java/com/trilogyed/tasker/dao/TaskerDaoTest.java
+++ Summative_Assessments/U2M1-Summative-Lora-Brian/tasker-service/src/test/java/com/trilogyed/tasker/dao/TaskerDaoTest.java
@@ -1,11 +1,14 @@
 package com.trilogyed.tasker.dao;
 
+import com.trilogyed.tasker.model.Task;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -17,7 +20,10 @@ public class TaskerDaoTest {
 
     @Before
     public void setUp() throws Exception {
-
+        List<Task> tasks = taskerDao.getAllTasks();
+        for (Task task : tasks) {
+            taskerDao.deleteTask(task.getId());
+        }
     }
 
     @Test
