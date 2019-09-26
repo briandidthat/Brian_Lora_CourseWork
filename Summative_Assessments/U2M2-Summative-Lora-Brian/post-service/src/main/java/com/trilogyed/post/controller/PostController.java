8c97226de1ca086da4369c2795a6920f8d6f09c7
Blora616 <Blora616@gmail.com>
2019-09-26 13:05:42 -0400

    fix

--- Summative_Assessments/U2M2-Summative-Lora-Brian/post-service/src/main/java/com/trilogyed/post/controller/PostController.java
+++ Summative_Assessments/U2M2-Summative-Lora-Brian/post-service/src/main/java/com/trilogyed/post/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.trilogyed.post.controller;
 
 import com.trilogyed.post.dao.PostDao;
+import com.trilogyed.post.exception.NotFoundException;
 import com.trilogyed.post.model.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -12,12 +13,17 @@ public class PostController {
 
     @RequestMapping(value = "/posts", method = RequestMethod.POST)
     public Post createPost(@RequestBody Post post) {
-        return null;
+        post = postDao.addPost(post);
+        return post;
     }
 
     @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
     public Post getPost(@PathVariable int id) {
-        return null;
+        Post post = postDao.getPost(id);
+        if (post == null) {
+            throw new NotFoundException("Sorry, we do not have any posts with that id.");
+        }
+        return post;
     }
 
     @RequestMapping(value = "/posts/{id}", method = RequestMethod.PUT)
@@ -26,12 +32,13 @@ public class PostController {
             post.setPostId(id);
         }
         if (id != post.getPostId()) {
-
+            throw new NotFoundException("Sorry, we do not have any posts with that id.");
         }
+        postDao.updatePost(post);
     }
 
     @RequestMapping(value = "/posts/{id}", method = RequestMethod.DELETE)
     public void deletePost(@PathVariable int id) {
-
+        postDao.deletePost(id);
     }
 }
