4caa0d2a88c3cd6b0d2b7baf533d18e7823b5cf4
Blora616 <Blora616@gmail.com>
2019-09-26 10:26:04 -0400

    entry

--- Summative_Assessments/U2M2-Summative-Lora-Brian/comment-service/src/main/java/com/trilogyed/comment/util/messages/CommentEntry.java
+++ Summative_Assessments/U2M2-Summative-Lora-Brian/comment-service/src/main/java/com/trilogyed/comment/util/messages/CommentEntry.java
@@ -1,8 +1,75 @@
 package com.trilogyed.comment.util.messages;
 
-/*Task:
- *
- */
-public class CommentEntry {
 
+public class CommentEntry {
+    private int commentId;
+    private int postId;
+    private int createDate;
+    private String commenterName;
+    private String comment;
+
+    public CommentEntry(int postId, int createDate, String commenterName, String comment) {
+        this.postId = postId;
+        this.createDate = createDate;
+        this.commenterName = commenterName;
+        this.comment = comment;
+    }
+
+    public CommentEntry() {
+    }
+
+
+    // SETTERS
+    public void setCommentId(int commentId) {
+        this.commentId = commentId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public void setCreateDate(int createDate) {
+        this.createDate = createDate;
+    }
+
+    public void setCommenterName(String commenterName) {
+        this.commenterName = commenterName;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+
+    // GETTERS
+    public int getCommentId() {
+        return commentId;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public int getCreateDate() {
+        return createDate;
+    }
+
+    public String getCommenterName() {
+        return commenterName;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    @Override
+    public String toString() {
+        return "CommentEntry{" +
+                "commentId=" + commentId +
+                ", postId=" + postId +
+                ", createDate=" + createDate +
+                ", commenterName='" + commenterName + '\'' +
+                ", comment='" + comment + '\'' +
+                '}';
+    }
 }
