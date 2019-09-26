f9c5340a9704acff6e0fd84073bc480c29d557d9
Blora616 <Blora616@gmail.com>
2019-09-26 15:19:52 -0400

    fix comment client

--- Summative_Assessments/U2M2-Summative-Lora-Brian/stwitter-service/src/main/java/com/trilogyed/stwitter/util/feign/CommentClient.java
+++ Summative_Assessments/U2M2-Summative-Lora-Brian/stwitter-service/src/main/java/com/trilogyed/stwitter/util/feign/CommentClient.java
@@ -9,17 +9,11 @@ import java.util.List;
 
 @FeignClient(name = "comment-client")
 public interface CommentClient {
-    @PostMapping("/comments")
-    Comment createComment(@RequestBody Comment comment);
-
     @GetMapping("/comments")
     List<Comment> getAllComments();
 
     @GetMapping("/comments/{id}")
     Comment findCommentById(@PathVariable int id);
-
-    @PutMapping("/comments/{id}")
-    void updateComment(@PathVariable int id, @RequestBody Comment comment);
 
     @DeleteMapping("/comments/{id}")
     void deleteComment(@PathVariable int id);
