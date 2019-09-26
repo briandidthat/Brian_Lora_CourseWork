8f7c1c8ca57a59bfb0d78829391e61582f9610de
Blora616 <Blora616@gmail.com>
2019-09-26 10:45:52 -0400

    controller

--- Summative_Assessments/U2M2-Summative-Lora-Brian/comment-service/src/main/java/com/trilogyed/comment/MessageListener.java
+++
@@ -1,8 +0,0 @@
-package com.trilogyed.comment;
-
-/*Task:
- *
- */
-public class MessageListener {
-
-}
--- Summative_Assessments/U2M2-Summative-Lora-Brian/comment-service/src/main/java/com/trilogyed/comment/controller/CommentController.java
+++ Summative_Assessments/U2M2-Summative-Lora-Brian/comment-service/src/main/java/com/trilogyed/comment/controller/CommentController.java
@@ -1,7 +1,14 @@
 package com.trilogyed.comment.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.trilogyed.comment.dao.CommentDao;
+import com.trilogyed.comment.exception.NotFoundException;
+import com.trilogyed.comment.model.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 /*Task:
  *
@@ -9,5 +16,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/comments")
 public class CommentController {
+
+    @Autowired
+    CommentDao commentDao;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Comment createComment(@RequestBody @Valid Comment comment) {
+        return commentDao.addComment(comment);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Comment> findAllComments() {
+        return commentDao.getAllComments();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Comment findCommentById(@PathVariable int id) {
+        return commentDao.getComment(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateComment(@PathVariable int id, @RequestBody Comment comment) {
+        if (comment.getCommentId() == 0) {
+            comment.setCommentId(id);
+        }
+        if (id != comment.getCommentId()) {
+            throw new NotFoundException("The path id and commentId do not match.");
+        }
+        commentDao.updateComment(comment);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteComment(@PathVariable int id) {
+        commentDao.deleteComment(id);
+    }
 
 }
